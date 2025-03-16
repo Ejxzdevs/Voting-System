8fbde4358a62d01f4688a30d6bf38773fe7bebbb
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-03-17 01:58:07 +0800

    refactor

--- index.php
+++ index.php
@@ -1,25 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title></title>
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
-<body>
-  <div class="container">
-    <div class="row mt-5">
-      <div class="col-md-6">
-        <a href="candidates.php" class="btn btn-primary btn-lg btn-block">ADMIN</a>
-      </div>
-      <div class="col-md-6">
-        <a href="voter_register.php" class="btn btn-secondary btn-lg btn-block">User</a>
-      </div>
-    </div>
-  </div>
-
-  <!-- Bootstrap JS (optional) -->
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</body>
-</html>
+<?php 
+header("Location: voter_register.php");
+exit();
