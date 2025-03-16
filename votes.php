604cbc9405971dfba098d822bbffe670ce3500c2
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-03-17 02:13:31 +0800

    feat: Add session tracking for voted positions and disable button
    - Implemented session-based tracking of positions that the user has already voted for.
    - Disabled the vote button for positions where the user has already cast their vote, preventing multiple votes in the same position.

--- votes.php
+++ votes.php
@@ -1,17 +1,25 @@
 <?php
+session_start();
 require_once 'config/connection.php';
 
 if (isset($_GET['candidate_id']) && isset($_GET['voter_id'])) {
-$cid= $_GET['candidate_id'];
-$vid= $_GET['voter_id'];
+$cid = $_GET['candidate_id'];
+$vid = $_GET['voter_id'];
+$position = $_GET['position'];
 
 $stmt = $conn->prepare("INSERT INTO votes (candidate_id,voter_id) VALUES (?,?)");
-// Bind parameters
 $stmt->bindParam(1, $cid);
 $stmt->bindParam(2, $vid);
 $stmt->execute();
-header("location: voter.php");
 
+
+if (isset($_SESSION['position_array']) && is_array($_SESSION['position_array'])) {
+    $_SESSION['position_array'][] = $position;
+} else {
+    $_SESSION['position_array'] = [$position];
+}
+
+header("location: voter.php");
 
 }
 
