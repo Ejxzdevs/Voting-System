4f9dbcde7407cdf06130039721bd13b0a2a6ee80
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-03-18 00:24:12 +0800

    feat(voting): add getVotes method to display candidates chosen by voter

--- services/votes_service.php
+++ services/votes_service.php
@@ -1,4 +1,24 @@
-<?php 
-$sql_fetch_data = "SELECT * FROM votes ";
+<?php
+global $conn;
+
+$sql_fetch_data = "SELECT * FROM votes";
 $stmt = $conn->query($sql_fetch_data);
 $votes = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+function getVotes($id) {
+    global $conn;
+    $sql_fetch_votes = "SELECT *,
+    (SELECT name FROM candidates WHERE id = votes.candidate_id) AS candidate_name,
+    (SELECT image_url FROM candidates WHERE id = votes.candidate_id) AS image,
+    (SELECT position_name 
+     FROM positions 
+     WHERE position_id = (SELECT position_id FROM candidates WHERE id = votes.candidate_id)) AS position_name
+FROM votes 
+WHERE voter_id = :voter_id";
+    $stmt = $conn->prepare($sql_fetch_votes);
+    $stmt->bindParam(':voter_id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $data;
+}
+
