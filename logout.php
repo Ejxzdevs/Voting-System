0103b0610a266862cb26ec4a499d185b90031840
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-03-17 21:27:33 +0800

    feat: redirect on logout based on user role

--- logout.php
+++ logout.php
@@ -1,6 +1,10 @@
-<?php 
+<?php
 session_start();
-session_unset(); 
-header("Location: index.php");
-exit(); 
+session_unset();
 
+if (isset($_GET['role']) && $_GET['role'] === 'admin') {
+    header("Location: login.php");
+} else {
+    header("Location: index.php");
+}
+exit();
